bb6a661d12c0a0888c89c5d4436c6195d63ff18d
escowin <edwin@escowinart.com>
2023-11-16 23:25:42 -0600

    logged char strings from utf-16 code units values

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,31 @@
+// dom variables
+const html = {
+  headerEl: document.querySelector("h1"),
+  copyrightEl: document.getElementById("copyright"),
+};
+
+// starts up app logic
 function init() {
-  const copyrightEl = document.getElementById("copyright");
-  const headerEl = document.querySelector("h1");
+  credits(html);
+
+  // Returns char strings from UTF-16 code units
+  const x = String.fromCharCode(48, 57, 65, 90, 97, 122);
+  console.log(x); // 0-9, A-Z, a-z
+}
+
+// displays app details in the console & browser
+function credits({ copyrightEl, headerEl }) {
   const date = new Date().getFullYear();
   const info = {
+    github: `https://github.com/escowin/`,
     app: "caesar-cipher",
     v: "0.0.1",
-    github: `https://github.com/escowin/`,
     copyright: `\u00a9 ${date} Edwin M. Escobar`,
+    link: () => info.github + info.app 
   };
 
-  console.log(`${info.copyright}\n${info.github + info.app}`);
-  copyrightEl.setAttribute("href", info.github + info.app);
+  console.log(`${info.copyright}\n${info.link()}`);
+  copyrightEl.setAttribute("href", info.link());
   copyrightEl.innerText = info.copyright;
   headerEl.innerText = `${info.app} ${info.v}`;
 }
